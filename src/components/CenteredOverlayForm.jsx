45e66652e38be5bb4f5e911fd31c117f7f7698c1
Conner <dudrjs145@naver.com>
2024-08-16 22:39:13 +0900

    CreateGroup Css 작성

--- src/components/CenteredOverlayForm.jsx
+++ src/components/CenteredOverlayForm.jsx
@@ -1,5 +1,32 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import styled from "styled-components";
+import { OverlayWrapper } from "./shared/OverlayWrapper";
 
-export const CenteredOverlayForm = () => {
-  return <div>CreateGroup Component</div>;
+export const CenteredOverlayForm = ({ children }) => {
+  return (
+    <StyledCenteralizedContainer>
+      <StyledHeader>Dutch Pay</StyledHeader>
+      <OverlayWrapper>{children}</OverlayWrapper>
+    </StyledCenteralizedContainer>
+  );
 };
+
+const StyledHeader = styled.h1`
+  font-weight: 700;
+  letter-spacing: 10px;
+  color: slateblue;
+  text-align: center;
+  margin-bottom: 0%.8em;
+`;
+
+const StyledCenteralizedContainer = styled(Container)`
+  width: 50%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0px;
+  gap: 10px;
+`;
--- src/components/CreateGroup.jsx
+++ src/components/CreateGroup.jsx
@@ -4,6 +4,7 @@ import { Container, Row, Button, Form } from "react-bootstrap";
 import { useRecoilState } from "recoil";
 import { groupNameState } from "../state/groupName";
 import { useState } from "react";
+import styled from "styled-components";
 
 export const CreateGroup = () => {
   const [validate, setValidate] = useState(false);
@@ -23,33 +24,65 @@ export const CreateGroup = () => {
   };
 
   return (
-    <div>
-      <h1>Dutch Pay</h1>
-      {/* <CenteredOverlayForm></CenteredOverlayForm> */}
+    <CenteredOverlayForm>
       <Container>
         <Form validated={validate} onSubmit={handleSubmit}>
-          <Row>
-            <h2>먼저, 더체 페이할 그룹의 이름을 정해볼까요?</h2>
-          </Row>
-          <Row>
-            <Form.Group>
-              <Form.Control
-                type="text"
-                required
-                placeholder="2022 제주도 여행"
-                value={groupName || ""}
-                onChange={(e) => setGroupName(e.target.value)}
-              />
-              <Form.Control.Feedback type="invalid" data-valid={validGroupName}>
-                그룹 이름을 입력해 주세요.
-              </Form.Control.Feedback>
-            </Form.Group>
-          </Row>
-          <Row>
-            <Button type="submit">저장</Button>
-          </Row>
+          <StyledRow>
+            <Row className="align-content-start">
+              <StyledH2>먼저, 더체 페이할 그룹의 이름을 정해볼까요?</StyledH2>
+            </Row>
+            <Row className="align-items-center">
+              <Form.Group>
+                <Form.Control
+                  type="text"
+                  required
+                  placeholder="2022 제주도 여행"
+                  value={groupName || ""}
+                  onChange={(e) => setGroupName(e.target.value)}
+                />
+                <Form.Control.Feedback
+                  type="invalid"
+                  data-valid={validGroupName}
+                >
+                  그룹 이름을 입력해 주세요.
+                </Form.Control.Feedback>
+              </Form.Group>
+            </Row>
+            <Row className="align-items-end">
+              <StyledButton>저장</StyledButton>
+            </Row>
+          </StyledRow>
         </Form>
       </Container>
-    </div>
+    </CenteredOverlayForm>
   );
 };
+
+const StyledH2 = styled.h2`
+  text-align: right;
+  font-weight: 700;
+  line-height: 1.2;
+  overflow-wrap: break-word;
+  word-break: keep-all;
+`;
+
+const StyledButton = styled(Button).attrs({
+  type: "submit",
+})`
+  width: 60%;
+  margin: 0 auto;
+  background-color: #6610f2;
+  border-radius: 8px;
+  border: none;
+
+  &:hover {
+    background-color: #6610f2;
+    filter: brightness(80%);
+  }
+`;
+
+const StyledRow = styled(Row)`
+  align-items: center;
+  justify-content: center;
+  height: 60vh;
+`;
